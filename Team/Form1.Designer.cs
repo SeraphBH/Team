aea6dffac56be150c3ce7938a9d115be1d787ba7
Seraph <seraphbgi@gmail.com>
2016-06-19 08:38:00 +0200

    Ajout Team

---
+++ Team/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Team
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Homme";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox2.Location = new System.Drawing.Point(217, 16);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Femme";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ajouter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(243, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Ajouter";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(14, 75);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(135, 277);
+            this.listBox1.TabIndex = 4;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(181, 75);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(135, 277);
+            this.listBox2.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button3.FlatAppearance.BorderSize = 2;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Location = new System.Drawing.Point(12, 358);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(304, 30);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Générer";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Honeydew;
+            this.label1.Location = new System.Drawing.Point(11, 391);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Couple";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(14, 407);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(303, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(328, 444);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Team Builder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Team/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Team
+{
+    public partial class Form1 : Form
+    {
+     
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.GotFocus += new EventHandler(textBox1_GotFocus);
+            textBox1.LostFocus += new EventHandler(textBox1_LostFocus);
+            textBox2.GotFocus += new EventHandler(textBox2_GotFocus);
+            textBox2.LostFocus += new EventHandler(textBox2_LostFocus);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(CheckEnter1);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(CheckEnter2);
+        }
+
+        private void CheckEnter1(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                if (textBox1.Text != "" && textBox1.Text != "Homme")
+                {
+                    listBox1.Items.Add(textBox1.Text);
+                    textBox1.Text = "";
+                }
+                
+            }
+        }
+        private void CheckEnter2(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                if (textBox2.Text != "" && textBox2.Text != "Femme")
+                {
+                    listBox2.Items.Add(textBox2.Text);
+                    textBox2.Text = "";
+                }
+               
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text != "" && textBox1.Text != "Homme")
+            {
+                listBox1.Items.Add(textBox1.Text);
+                textBox1.Text = "";
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text != "" && textBox2.Text != "Femme")
+            {
+                listBox2.Items.Add(textBox2.Text);
+                textBox2.Text = "";
+            }
+        }
+
+        private void textBox1_GotFocus(Object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+         }
+        private void textBox1_LostFocus(Object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            textBox1.Text = "Homme";
+        }
+        private void textBox2_GotFocus(Object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+        }
+        private void textBox2_LostFocus(Object sender, EventArgs e)
+        {
+            if (textBox2.Text == "")
+            textBox2.Text = "Femme";
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public int Azdine(int limite)
+        {
+                 int nProcessId = Process.GetCurrentProcess().Id;
+            Random randomClass = new Random(DateTime.Now.Millisecond * nProcessId);
+            int preRandom = randomClass.Next(0, Int32.MaxValue);
+            int randomNumber = randomClass.Next(0, Int32.MaxValue) * preRandom;
+
+            randomNumber = (randomNumber < 0) ? (randomNumber * (-1)) : (randomNumber);
+            randomNumber %= limite;
+
+            return randomNumber;
+        }
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            String Homme = "";
+            String Femme = "";
+          
+            //int[] Tab= new int[10]{1,9,9,9,5,8,9,8,7,4};
+            int Homme1 = listBox1.Items.Count;
+            int Femme1 = listBox2.Items.Count;
+
+          Random randomClass = new Random(DateTime.Now.Millisecond);
+          int A = Azdine(Homme1);
+          int B = Azdine(Femme1);
+            
+            //int A = randomClass.Next(0, Homme1);
+            //int B = randomClass.Next(0, Femme1);
+            if (Homme1 > 0 && Femme1 > 0)
+                label1.Text = "Couple Mixte";
+            else if (Homme1 == 0 || Femme1 == 0)
+                label1.Text = "Couple Non Mixte";
+
+
+            if (Homme1 == 0 && Femme1 == 0)
+            label1.Text = "Plus de couple Possible";
+
+            else if (Homme1 == 1 && Femme1 == 0)
+            label1.Text = "Plus de couple Possible";
+
+            else if (Homme1 == 0 && Femme1 == 1)
+            label1.Text = "Plus de couple Possible";
+                
+            else if (Homme1 >= 1 && Femme1 >= 1)
+            {
+             Homme = listBox1.Items[A].ToString();
+             textBox3.Text = Homme + " ";
+             listBox1.Items.RemoveAt(A);
+             Femme = listBox2.Items[B].ToString();
+             textBox3.Text += Femme + " ";
+             listBox2.Items.RemoveAt(B);
+             
+            }
+            else if (Femme1 >= 2)
+            {
+                Femme = listBox2.Items[B].ToString();
+                textBox3.Text = Femme + " ";
+                listBox2.Items.RemoveAt(B);
+
+                Femme1 = listBox1.Items.Count;
+                B = Azdine(Femme1);
+                //B = randomClass.Next(0, Femme1);
+
+                Femme = listBox2.Items[B].ToString();
+                textBox3.Text += Femme + " ";
+                listBox2.Items.RemoveAt(B);
+            }
+            else if (Homme1 >= 2)
+            {
+                Homme = listBox1.Items[A].ToString();
+                textBox3.Text = Homme + " ";
+                listBox1.Items.RemoveAt(A);
+
+                Homme1 = listBox1.Items.Count;
+                A = Azdine(Homme1);
+                //A = randomClass.Next(0, Homme1);
+
+                Homme = listBox1.Items[A].ToString();
+                textBox3.Text += Homme + " ";
+                listBox1.Items.RemoveAt(A);
+            }
+            
+        
+        }
+    }
+}
